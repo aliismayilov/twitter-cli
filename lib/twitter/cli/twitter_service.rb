a43a5904b9a4ab99379a0f85ce85d0c44d0d857b
Ali Ismayilov <ali@ismailov.info>
2014-09-07 09:32:56 -0300

    TwitterService#base64_credentials

--- lib/twitter/cli/twitter_service.rb
+++ lib/twitter/cli/twitter_service.rb
@@ -2,8 +2,19 @@ module Twitter
   module Cli
     class TwitterService
       include HTTParty
-      base_uri 'https://api.twitter.com/1.1'
+      base_uri 'https://api.twitter.com'
       format :json
+      attr_reader :base64_credentials
+
+      def initialize consumer_key, consumer_secret
+        @consumer_key     = consumer_key
+        @consumer_secret  = consumer_secret
+      end
+
+      def base64_credentials
+        url_encoded_credentials = "#{URI.encode(@consumer_key)}:#{URI.encode(@consumer_secret)}"
+        Base64.encode64(url_encoded_credentials).gsub("\n", '')
+      end
     end
   end
 end
--- spec/twitter/cli/twitter_service_spec.rb
+++ spec/twitter/cli/twitter_service_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 describe Twitter::Cli::TwitterService do
   describe 'config' do
     subject { Twitter::Cli::TwitterService }
-    its(:base_uri) { is_expected.to eql 'https://api.twitter.com/1.1' }
+    its(:base_uri) { is_expected.to eql 'https://api.twitter.com' }
     its(:format)   { is_expected.to eql :json }
+  end
+
+  describe 'getting access token' do
+    subject(:twitter_service) { Twitter::Cli::TwitterService.new('xvz1evFS4wEEPTGEFPHBog', 'L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg') }
+
+    it 'encodes consumer key and secret in base64 format' do
+      expect(twitter_service.base64_credentials).to eql 'eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw=='
+    end
   end
 end
